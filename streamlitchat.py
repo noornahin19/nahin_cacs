70c958c713342355b7d4b23dbd1e679f36795b6f
noornahin19 <noornahin19@gmail.com>
2026-09-04 10:47:15 +0530

    Update chat input prompt

--- streamlitchat.py
+++ streamlitchat.py
@@ -1,5 +1,5 @@
 import streamlit as st
-a=st.chat_input("enter your command")
+a=st.chat_input("enter your command or see you again")
 if a: 	
 	st.chat_message("user").write(a)
 	if a.lower()== "hi":
